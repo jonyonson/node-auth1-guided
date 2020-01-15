dda4a3aebf85ef14887f9b6dfcaf8736a5458b4f
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-01-14 19:45:25 -0600

    updates

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,35 +1,37 @@
-const express = require("express")
-const usersModel = require("../users/users-model")
+const bcrypt = require("bcryptjs");
+const express = require("express");
+const usersModel = require("../users/users-model");
 
-const router = express.Router()
+const router = express.Router();
 
 router.post("/register", async (req, res, next) => {
   try {
-    const saved = await usersModel.add(req.body)
-    
-    res.status(201).json(saved)
+    const saved = await usersModel.add(req.body);
+
+    res.status(201).json(saved);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 router.post("/login", async (req, res, next) => {
   try {
-    const { username, password } = req.body
-    const user = await usersModel.findBy({ username }).first()
+    const { username, password } = req.body;
+    const user = await usersModel.findBy({ username }).first();
+    const passwordValid = await bcrypt.compare(password, user.password);
 
-    if (user) {
+    if (user && passwordValid) {
       res.status(200).json({
-        message: `Welcome ${user.username}!`,
-      })
+        message: `Welcome ${user.username}!`
+      });
     } else {
       res.status(401).json({
-        message: "Invalid Credentials",
-      })
+        message: "Invalid Credentials"
+      });
     }
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
--- users/users-model.js
+++ users/users-model.js
@@ -1,32 +1,33 @@
-const db = require("../database/dbConfig")
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig");
 
 function find() {
-  return db("users")
-    .select("id", "username")
+  return db("users").select("id", "username");
 }
 
 function findBy(filter) {
   return db("users")
     .where(filter)
-    .select("id", "username", "password")
+    .select("id", "username", "password");
 }
 
 async function add(user) {
-  const [id] = await db("users")
-    .insert(user)
- 
-  return findById(id)
+  user.password = await bcrypt.hash(user.password, 14);
+
+  const [id] = await db("users").insert(user);
+
+  return findById(id);
 }
 
 function findById(id) {
   return db("users")
     .where({ id })
-    .first("id", "username")
+    .first("id", "username");
 }
 
 module.exports = {
   add,
   find,
   findBy,
-  findById,
-}
+  findById
+};
